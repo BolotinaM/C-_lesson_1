2c71ddb2ec9a0ea3f135dfea01e4738c4495536b
Mariia <bolotina.23reg@yandex.ru>
2022-11-25 22:37:19 +0300

    Добавили решение задачи № 4

---
+++ 1_lesson/1_4/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу вычисления модуля числа
+
+Console.WriteLine("Введите целое числое");
+int N = int.Parse(Console.ReadLine());
+
+if (N < 0)
+{
+    N = N*(-1);
+    Console.WriteLine(N);
+}
+else
+{
+    Console.WriteLine(N);
+}
