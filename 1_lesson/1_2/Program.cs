c34d4881c4842327a6e5a6da00f68c9ce1dc45ea
Mariia <bolotina.23reg@yandex.ru>
2022-11-25 22:51:08 +0300

    Добавили решение задачи № 2

---
+++ 1_lesson/1_2/Program.cs
@@ -0,0 +1,37 @@
+﻿// Создайте программу, которая будет выдавать название дня недели по заданному номеру.
+
+Console.WriteLine("Введите порядковый номер дня недели, где 1 - понедельник, 7 - воскресенье");
+int NumDay = int.Parse(Console.ReadLine());
+
+if (NumDay == 1)
+{
+    Console.WriteLine("Monday");
+}
+if (NumDay == 2)
+{
+    Console.WriteLine("Tuesday");
+}
+if (NumDay == 3)
+{
+    Console.WriteLine("Wednesday");
+}
+if (NumDay == 4)
+{
+    Console.WriteLine("Thursday");
+}
+if (NumDay == 5)
+{
+    Console.WriteLine("Friday");
+}
+if (NumDay == 6)
+{
+    Console.WriteLine("Saturday");
+}
+if (NumDay == 7)
+{
+    Console.WriteLine("Sunday");
+}
+else 
+{
+    Console.WriteLine("Try again");   
+}
