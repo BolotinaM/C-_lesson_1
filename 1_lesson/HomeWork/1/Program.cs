444facc62aa2b04884025720263d079488ae91fd
Mariia <bolotina.23reg@yandex.ru>
2022-11-25 23:12:22 +0300

    Добавили решение домашней задачи № 1

---
+++ 1_lesson/HomeWork/1/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу, которая на вход принимает два числа
+// и выдаёт, какое число большее, а какое - меньшее.
+
+int Max = 0;
+int Min = 0;
+
+Console.WriteLine("Введите значение первого целого числа");
+int N1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите значение второго целого числа");
+int N2 = int.Parse(Console.ReadLine());
+
+if (N1 > N2)
+{
+    Max = N1;
+    Min = N2;
+    Console.Write("Максимальное значение = ");
+    Console.WriteLine(Max);
+    Console.Write("Минимальное значение = ");
+    Console.WriteLine(Min);
+}
+if (N2 > N1)
+{
+    Max = N2;
+    Min = N1;
+    Console.Write("Максимальное значение = ");
+    Console.WriteLine(Max);
+    Console.Write("Минимальное значение = ");
+    Console.WriteLine(Min);
+}
+else
+{
+    Console.WriteLine("Значения равнозначны");
+}
